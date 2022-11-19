776f87951c6274dbe0f54cfdd207010c6714d7f1
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-11-19 21:04:06 +0630

    learn const

---
+++ constant/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int AGE = 33; // you can not change and reasign const variable;
+    double myPrice = 0.22;
+    string myName = "htet";
+    cout << AGE << endl;
+    cout << myPrice << endl;
+    cout << myName << endl;
+    return 0;    
+}
